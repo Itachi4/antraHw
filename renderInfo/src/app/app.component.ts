bb19684e80ffe6f86d7396a9e8c21cc23987798a
Itachi4 <klausmikaelson560@gmail.com>
2024-07-31 13:29:19 -0400

    renderInfo

---
+++ renderInfo/src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+interface levels {
+  [key: string]: {
+    [key: string]: string[];
+  };
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'renderInfo';
+
+  data = [
+    ['Foley', 'Chemicals', 'CHEM'],
+    ['Foley', 'Chemicals', 'CTO'],
+    ['Foley', 'Chemicals', 'LK'],
+    ['Foley', 'Chemicals', 'R8'],
+    ['Foley', 'Chemicals', 'WT'],
+    ['Foley', 'Finishing', 'LB2'],
+    ['Foley', 'Finishing', 'LB4'],
+    ['Foley', 'Finishing', 'RW1'],
+    ['Foley', 'Finishing', 'RW2'],
+    ['Foley', 'Line 3', 'LN3'],
+    ['Foley', 'Line 3', 'Production Process'],
+    ['Foley', 'Line 4', 'LN4'],
+    ['Foley', 'Line 4', 'Prod Process'],
+    ['Foley', 'Mill General', 'Wastewater Treatment'],
+    ['Foley', 'Powerhouse', 'BB1'],
+    ['Foley', 'Powerhouse', 'BB2'],
+    ['Foley', 'Powerhouse', 'EV5'],
+    ['Foley', 'Powerhouse', 'FWE'],
+    ['Foley', 'Powerhouse', 'PB1'],
+    ['Foley', 'Powerhouse', 'PB2'],
+    ['Foley', 'Powerhouse', 'RB2'],
+    ['Foley', 'Powerhouse', 'RB3'],
+    ['Foley', 'Powerhouse', 'RB4'],
+    ['Foley', 'Powerhouse', 'TG2'],
+    ['Foley', 'Powerhouse', 'TG3'],
+    ['Foley', 'Powerhouse', 'TG4'],
+  ];
+
+  getLevels(): levels {
+    const order: levels= {};
+
+    this.data.forEach((item) => {
+      const [lv1, lv2, lv3] = item;
+
+      if (!order[lv1]) {
+        order[lv1] = {};
+      }
+
+      if (!order[lv1][lv2]) {
+        order[lv1][lv2] = [];
+      }
+
+      order[lv1][lv2].push(lv3);
+    });
+
+    return order;
+  }
+}
